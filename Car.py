340326b79a8ff72e69ed91e453cab212f9e353be
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2022-04-26 22:34:22 -0500

    add: types to the constructors parameters

--- Car.py
+++ Car.py
@@ -1,7 +1,8 @@
 class Car:
-  def __init__(self, name, color) -> None:
+  def __init__(self, name: str, color: str, length: int) -> None:
     self.name = name
     self.color = color
+    self.length = length
   
   def __str__(self) -> str:
     return 'Name: ' + self.name + ';Color: ' + self.color
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Car import Car
 
 def __init__():
-  toyota = Car('Toyota' ,'Red')
-  print(toyota)
+  toyota = Car('Toyota' ,'Red', 30)
+  print(type(toyota.length))
 
 __init__()
