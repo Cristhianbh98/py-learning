342a1809ab26201b4d8f16c1e91cdad48db0bb99
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2022-04-26 22:48:53 -0500

    feat: valid email with regex

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from Car import Car
+import re
+
+def is_email_valid(email: str) -> bool:
+  m = re.search('^[A-Za-z0-9.]+@[A-Za-z0-9.]+\.[A-Za-z0-9]{2,3}$', email)
+  return bool(m)
 
 def __init__():
-  toyota = Car('Toyota' ,'Red', 30)
-  print(type(toyota.length))
+  email = input('Enter a email\n')
+  if(is_email_valid(email.strip())):
+    print(email + ' is a valid email')
+  else:
+    print(email + ' is not a valid email')
 
 __init__()
